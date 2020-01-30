0a0fb7fd0d2ec88543fad83c7be91b2c744824c4
itggot-anders-bohlin <anders.bohlin@elev.ga.ntig.se>
2020-01-30 10:28:52 +0100

    har gjort filsystem och inloggning

---
+++ app.rb
@@ -0,0 +1,90 @@
+require 'sinatra'
+require 'slim'
+require 'BCrypt'
+require 'SQLite3'
+
+enable :sessions
+
+get('/') do
+    slim(:start)
+end
+
+get('/users/index') do
+
+    slim(:"users/index")
+end
+
+post('/create') do
+    db = SQLite3::Database.new("db/storprojekt.db")
+    username = params["username"]
+    password = params["password"]
+    confirm_password = params["confirm_password"]
+    result = db.execute("SELECT * FROM users WHERE username=?", username)
+
+    if result.empty?
+        if password == confirm_password
+            password_digest = BCrypt::Password.create(password)
+            db.execute("INSERT INTO users(username, password) VALUES (?,?)", [username, password_digest])
+            session[:user_id] = db.execute("SELECT ID FROM users WHERE username=?", [username])
+            session[:username] = username
+            redirect('/register_confirmation')
+        else
+            redirect('/error')
+            
+        end
+    else
+        redirect('/error')
+    end
+
+    redirect('/users/index')
+end
+
+get('/register_confirmation') do
+    slim(:register_confirmation)
+end
+
+get('/users/new') do
+    slim(:"users/new")
+end
+
+post('/login') do
+    db = SQLite3::Database.new("db/storprojekt.db")
+    username = params["username"]
+    password = params["password"]
+    db.results_as_hash = true
+    result = db.execute("SELECT ID, password FROM users WHERE username=?", [username])
+    if result.empty?
+        redirect('/error')
+    end
+    user_id = result.first["ID"]
+    password_digest = result.first["password"]
+    if BCrypt::Password.new(password_digest) == password
+        session[:username] = username
+        session[:user_id] = user_id
+        redirect("/shop/index")
+    end
+    
+end
+
+get('/shop/index') do
+    db = SQLite3::Database.new("db/storprojekt.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM items")
+    if result != []
+        slim(:"shop/index", locals:{items:result})
+    else
+        slim(:error)
+    end
+end
+
+
+
+
+
+
+
+
+
+get('/error') do
+    slim(:error)
+end
--- projektplan.md
+++ projektplan.md
@@ -1,7 +1,7 @@
 # Projektplan
 
 ## 1. Projektbeskrivning (Beskriv vad sidan ska kunna göra)
-    Den ska tillhandahålla information om alla signifikanta karaktärer samt deras relationer i bokserien "the malazan book of the fallen"
+    Jag skall göra en webshop.
 ## 2. Vyer (visa bildskisser på dina sidor)
 ## 3. Databas med ER-diagram (Bild)
 ## 4. Arkitektur (Beskriv filer och mappar - vad gör/inehåller de?)
