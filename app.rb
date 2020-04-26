ae9fef11f1e87888ace7d7fac4e29519b33217c2
itggot-anders-bohlin <60005983+itggot-anders-bohlin@users.noreply.github.com>
2020-04-26 20:34:21 +0200

    yardocat

--- app.rb
+++ app.rb
@@ -4,19 +4,28 @@ require 'BCrypt'
 require 'SQLite3'
 require_relative './model.rb'
 enable :sessions
+include Model
 timearray = []
-before do
-   session[:user_id] = 1 #ta bort innan inlämning
-end
    
+# Display Landing Page
+#
 get('/') do
     slim(:start)
 end
 
+# Display page for creating an account
+#
 get('/users/index') do
     slim(:"users/index")
 end
 
+# Creates a new account and redirects to '/register_confirmation'
+#
+# @param [String] username, the username of the user
+# @param [String] password, the password of the user
+# @param [String] confirm_password, a confirmation of the user's password
+#
+# @see Model#register_user
 post('/create') do
     username = params["username"]
     password = params["password"]
@@ -24,22 +33,32 @@ post('/create') do
     register_user(username, password, confirm_password)
 end
 
+# Display page that confirms registration of an account
+#
 get('/register_confirmation') do
     slim(:register_confirmation)
 end
 
+# Display page for logging in to an account
+#
 get('/users/new') do
     slim(:"users/new")
 end
 
+# Logs in to an account and redirects to '/shop/index'
+#
+# @param [String] username, the username of the user
+# @param [String] password, the password of the user
+#
+# @see Model#login_user
 post('/login') do
     username = params["username"]
     password = params["password"]
     login_user(username, password, timearray)
 end
 
-
-
+#Checks to see that a user is logged in before accessing sites that they shouldn't
+#
 before("/shop/:id") do
     if session[:user_id] == nil
         session[:error] = "Logga in för att se denna sida."
@@ -47,42 +66,73 @@ before("/shop/:id") do
     end
 end
 
+# Display page for adding items to your cart
+#
+# @see Model#vew_shop
 get('/shop/index') do
     info = view_shop()
     slim(:"shop/index", locals:{items:info[1], admin:info[0]})
 end
 
+# Adds item to cart and redirects to '/shop/index'
+#
+# @param [Integer] :id, the id of the item
+# 
+# @see Model#add_to_cart
 post('/shop/:id/add') do
     item_id = params[:id].to_i
     add_to_cart(item_id)
     redirect('/shop/index')
 end
 
+# Display page with your cart
+#
+# @see Model#view_cart
 get('/shop/show') do
     info = view_cart()
     slim(:"shop/show", locals:{items:info[0], total:info[1]})
 end
 
+# Remove item from your cart and redirect to '/shop/show'
+#
+# @param [Integer] :id, the id of the item
+#
+# @see Model#remove_from_cart
 post('/shop/show/:id') do
     item_id = params[:id].to_i
     remove_from_cart(item_id)
     redirect('/shop/show')
 end
 
+# Adds all the items in your cart to your orders and redirects to '/shop/new'
+#
+# @see Model#add_to_order
 post('/shop/order') do
     add_to_order()
     redirect('/shop/new')
 end
 
+# Dispaly page for your orders
+#
+# @see Model#view_orders
 get('/shop/new') do
     orders = view_orders()
     slim(:"shop/new", locals:{orders:orders})
 end
 
+# Display page for editing the items in the shop
+#
 get('/shop/edit') do
     slim(:"shop/edit")
 end
 
+# Adds a new item to the shop and redirects to '/shop/edit'
+#
+# @param [String] title, the title of the item
+# @param [Integer] price, the price of the item
+# @param [Integer] amount, the amount of the item
+#
+# @see Model#add_to_shop
 post('/shop/add') do
     title=params["title"]
     price=params["price"]
@@ -94,6 +144,13 @@ post('/shop/add') do
     redirect('/shop/edit')
 end
 
+# Updates an item already in the shop and redirects to '/shop/edit'
+#
+# @param [String] title, the title of the item
+# @param [Integer] price, the price of the item
+# @param [Integer] amount, the amount of the item
+#
+# @see Model#update_shop
 post('/shop/update') do
     title=params["title"]
     price=params["price"]
@@ -105,6 +162,11 @@ post('/shop/update') do
     redirect('/shop/edit')
 end
 
+# Removes an item from the shop and redirects to '/shop/edit'
+#
+# @param [String] title, the title of the item
+#
+# @see Model#delete_from_shop
 post('/shop/delete') do
     title = params["title"]
     if title == ""
@@ -114,6 +176,12 @@ post('/shop/delete') do
     redirect('/shop/edit')
 end
 
+# Removes the specified order and redirects to '/shop/edit'
+#
+# @param [Integer] user, the user wo placed the order
+# @param [Integer] order, the order that has been delivered.
+#
+# @see Model#deliver
 post('/shop/deliver') do
     user_id = params["user"]
     order_id = params["order"]
@@ -121,6 +189,8 @@ post('/shop/deliver') do
     redirect('/shop/edit')
 end
 
+# Display page with an error message
+#
 get('/error') do
     slim(:error)
 end
--- model.rb
+++ model.rb
@@ -1,152 +1,195 @@
-require 'sinatra'
-require 'slim'
-require 'BCrypt'
-require 'SQLite3'
+module Model
 
-def connect_to_db(path)
-    db = SQLite3::Database.new("db/storprojekt.db")
-    db.results_as_hash = true
-    return db
-end
+    # Connects to the database
+    # 
+    # @return 
+    def connect_to_db(path)
+        db = SQLite3::Database.new("db/storprojekt.db")
+        db.results_as_hash = true
+        return db
+    end
 
-def register_user(username, password, confirm_password)
-    db = connect_to_db("db/storprojekt.db")
-    result = db.execute("SELECT * FROM users WHERE username=?", username)
+    # Registers a user 
+    #
+    def register_user(username, password, confirm_password)
+        db = connect_to_db("db/storprojekt.db")
+        result = db.execute("SELECT * FROM users WHERE username=?", username)
 
-    if result.empty?
-        if password == confirm_password
-            password_digest = BCrypt::Password.create(password)
-            db.execute("INSERT INTO users(username, password, admin) VALUES (?,?,?)", [username, password_digest, 0])
-            result = db.execute("SELECT id FROM users WHERE username=?", [username])
-            session[:user_id] = result.first["id"]
-            session[:username] = username
-            redirect('/register_confirmation')
+        if result.empty?
+            if password == confirm_password
+                password_digest = BCrypt::Password.create(password)
+                db.execute("INSERT INTO users(username, password, admin) VALUES (?,?,?)", [username, password_digest, 0])
+                result = db.execute("SELECT id FROM users WHERE username=?", [username])
+                session[:user_id] = result.first["id"]
+                session[:username] = username
+                redirect('/register_confirmation')
+            else
+                session[:error] = "Dina lösenord matchar inte"
+                redirect('/error')
+            end
         else
-            session[:error] = "Dina lösenord matchar inte"
+            session[:error]="Det finns redan ett konto med detta användarnamn"
             redirect('/error')
         end
-    else
-        session[:error]="Det finns redan ett konto med detta användarnamn"
-        redirect('/error')
     end
-end
 
-def login_user(username, password, timearray)
-    db = connect_to_db("db/storprojekt.db")
-    result = db.execute("SELECT id, password FROM users WHERE username=?", [username])
-    if result.empty?
-        session[:error] = "Det finns inget konto med detta användarnamn"
-        redirect("/users/new")
-    end
-    user_id = result.first["id"]
-    password_digest = result.first["password"]
-    if BCrypt::Password.new(password_digest) == password
-        session[:username] = username
-        session[:user_id] = user_id
-        redirect("/shop/index")
-    else
-        session[:error] = "Fel lösenord"
-        timearray << Time.now.to_i
-        if timearray.length > 4 
-            if Time.now.to_i - timearray[0] < 10
-                time = Time.now.to_i
-                session[:error] = "Du har försökt för många lösenord på för kort tid, vänta 5 sekunder"
-                redirect("/users/new")
-                timearray = []
+    # logs a user in
+    #
+    def login_user(username, password, timearray)
+        db = connect_to_db("db/storprojekt.db")
+        result = db.execute("SELECT id, password FROM users WHERE username=?", [username])
+        if result.empty?
+            session[:error] = "Det finns inget konto med detta användarnamn"
+            redirect("/users/new")
+        end
+        user_id = result.first["id"]
+        password_digest = result.first["password"]
+        if BCrypt::Password.new(password_digest) == password
+            session[:username] = username
+            session[:user_id] = user_id
+            redirect("/shop/index")
+        else
+            session[:error] = "Fel lösenord"
+            timearray << Time.now.to_i
+            if timearray.length > 4 
+                if Time.now.to_i - timearray[0] < 10
+                    time = Time.now.to_i
+                    session[:error] = "Du har försökt för många lösenord på för kort tid, vänta 5 sekunder"
+                    redirect("/users/new")
+                    timearray = []
+                end
+                
+                timearray.shift
             end
             
-            timearray.shift
+            redirect("/users/new")
         end
         
-        redirect("/users/new")
     end
-    
-end
 
-
-def view_shop()
-    db = connect_to_db("db/storprojekt.db")
-    admin = db.execute("SELECT admin FROM users WHERE id = ?", session[:user_id])
-    result = db.execute("SELECT * FROM items")
-    return admin, result
-end
-
-def add_to_cart(item_id)
-    db = connect_to_db("db/storprojekt.db")
-    amount = db.execute("SELECT amount FROM cart WHERE item_id = ? AND user_id = ?", [item_id, session[:user_id]])
-    if amount == []
-        db.execute("INSERT INTO cart(item_id, user_id, amount) VALUES (?,?,?)", [item_id, session[:user_id]], 1)
-    else
-        amount = amount[0]["amount"] + 1
-        db.execute("UPDATE cart SET amount = ? WHERE item_id = ? AND user_id = ?", [amount,  item_id, session[:user_id]])
+    # Finds whether the current user is an admin and what items are currently in the shop
+    #
+    # @return [Hash] 
+    #   * admin [Integer] whether the current user is an admin
+    # @return [Hash] 
+    #   * id [Integer] the id of the item
+    #   * title [String] the title of the item
+    #   * amount [Integer] the amount of the item
+    #   * price [Integer] the price of the item
+    def view_shop()
+        db = connect_to_db("db/storprojekt.db")
+        admin = db.execute("SELECT admin FROM users WHERE id = ?", session[:user_id])
+        result = db.execute("SELECT * FROM items")
+        return admin, result
     end
-end
 
-def view_cart()
-    db = connect_to_db("db/storprojekt.db")
-    item = db.execute("SELECT item_id, amount FROM cart WHERE user_id = ?", session[:user_id])
-    result = []
-    totalprice = 0
-    item.each do |el|
-        info = db.execute("SELECT * FROM items WHERE id = ?", el["item_id"])
-        info[0][:amount] = el["amount"]
-        totalprice = totalprice + (info.first["price"].to_i * el["amount"])
-        result << info
-    end
-    return result, totalprice
-end
-
-def remove_from_cart(item_id)
-    db = connect_to_db("db/storprojekt.db")
-    db.execute("UPDATE cart SET amount = amount-1 WHERE item_id = ? AND user_id = ?", [item_id, session[:user_id]])
-    amount = db.execute("SELECT amount FROM cart WHERE item_id = ? AND user_id = ?", [item_id, session[:user_id]])
-    if amount[0]["amount"] == 0
-        db.execute("DELETE FROM cart WHERE item_id = ? AND user_id = ?", [item_id, session[:user_id]])
-    end
-end
-
-def add_to_order()
-    db = connect_to_db("db/storprojekt.db")
-    result = db.execute("SELECT * FROM cart WHERE user_id = ?", session[:user_id])
-    result.each do |el|
-        stock = db.execute("SELECT amount FROM items WHERE id = ?", el["item_id"])
-        if stock[0]["amount"].to_i >= el["amount"].to_i
-            db.execute("UPDATE items SET amount = amount - ? WHERE id = ?", [el["amount"], el["item_id"]])
+    # Adds items to the cart
+    #
+    def add_to_cart(item_id)
+        db = connect_to_db("db/storprojekt.db")
+        amount = db.execute("SELECT amount FROM cart WHERE item_id = ? AND user_id = ?", [item_id, session[:user_id]])
+        if amount == []
+            db.execute("INSERT INTO cart(item_id, user_id, amount) VALUES (?,?,?)", [item_id, session[:user_id]], 1)
         else
-            session[:error] = "En av varorna du försöker beställa är tyvärr slut i lagret."
-            redirect('/error')
+            amount = amount[0]["amount"] + 1
+            db.execute("UPDATE cart SET amount = ? WHERE item_id = ? AND user_id = ?", [amount,  item_id, session[:user_id]])
         end
-        db.execute("INSERT INTO orders(user_id, item_id, amount) VALUES (?,?,?)", [session[:user_id], el["item_id"], el["amount"]])
     end
-    db.execute("DELETE FROM cart WHERE user_id = ?", session[:user_id])
-end
 
-def view_orders()
-    db = connect_to_db("db/storprojekt.db")
-    orders = db.execute("SELECT * FROM orders WHERE user_id = ?", session[:user_id])
-    orders.each do |el|
-        item = db.execute("SELECT title FROM items WHERE id = ?", el["item_id"])
-        el["item_id"] = item[0]["title"]
+    # Finds items in your cart and calculates the total prce of them
+    #
+    # @return [Hash] 
+    #   * id [Integer] the id of the item
+    #   * title [String] the title of the item
+    #   * amount [Integer] the amount of the item
+    #   * price [Integer] the price of the item
+    #
+    # @return [Integer] totalprice, the total price of the items in the cart
+    def view_cart()
+        db = connect_to_db("db/storprojekt.db")
+        item = db.execute("SELECT item_id, amount FROM cart WHERE user_id = ?", session[:user_id])
+        result = []
+        totalprice = 0
+        item.each do |el|
+            info = db.execute("SELECT * FROM items WHERE id = ?", el["item_id"])
+            info[0][:amount] = el["amount"]
+            totalprice = totalprice + (info.first["price"].to_i * el["amount"])
+            result << info
+        end
+        return result, totalprice
     end
-    return orders
-end
 
-def add_to_shop(title, price, amount)
-    db = connect_to_db("db/storprojekt.db")
-    db.execute("INSERT INTO items(title, price, amount) VALUES (?,?,?)", [title, price, amount])
-end
+    # Removes an item from the cart
+    #
+    def remove_from_cart(item_id)
+        db = connect_to_db("db/storprojekt.db")
+        db.execute("UPDATE cart SET amount = amount-1 WHERE item_id = ? AND user_id = ?", [item_id, session[:user_id]])
+        amount = db.execute("SELECT amount FROM cart WHERE item_id = ? AND user_id = ?", [item_id, session[:user_id]])
+        if amount[0]["amount"] == 0
+            db.execute("DELETE FROM cart WHERE item_id = ? AND user_id = ?", [item_id, session[:user_id]])
+        end
+    end
 
-def update_shop(title, price, amount)
-    db = connect_to_db("db/storprojekt.db")
-    db.execute("UPDATE items SET price = ?, amount = ? WHERE title = ?", [price, amount, title])
-end
+    # Adds the items in the cart to an order and removes the amount of each item from the stock
+    #
+    def add_to_order()
+        db = connect_to_db("db/storprojekt.db")
+        result = db.execute("SELECT * FROM cart WHERE user_id = ?", session[:user_id])
+        result.each do |el|
+            stock = db.execute("SELECT amount FROM items WHERE id = ?", el["item_id"])
+            if stock[0]["amount"].to_i >= el["amount"].to_i
+                db.execute("UPDATE items SET amount = amount - ? WHERE id = ?", [el["amount"], el["item_id"]])
+            else
+                session[:error] = "En av varorna du försöker beställa är tyvärr slut i lagret."
+                redirect('/error')
+            end
+            db.execute("INSERT INTO orders(user_id, item_id, amount) VALUES (?,?,?)", [session[:user_id], el["item_id"], el["amount"]])
+        end
+        db.execute("DELETE FROM cart WHERE user_id = ?", session[:user_id])
+    end
 
-def delete_from_shop(title)
-    db = connect_to_db("db/storprojekt.db")
-    db.execute("DELETE FROM items WHERE title = ?", title)
-end
+    # Selects the orders of the current user
+    #
+    # @return [Hash] 
+    #   * id [Integer] the id of the item
+    #   * title [String] the title of the item
+    #   * amount [Integer] the amount of the item
+    #   * price [Integer] the price of the item
+    def view_orders()
+        db = connect_to_db("db/storprojekt.db")
+        orders = db.execute("SELECT * FROM orders WHERE user_id = ?", session[:user_id])
+        orders.each do |el|
+            item = db.execute("SELECT title FROM items WHERE id = ?", el["item_id"])
+            el["item_id"] = item[0]["title"]
+        end
+        return orders
+    end
 
-def deliver(user_id, order_id)
-    db = connect_to_db("db/storprojekt.db")
-    db.execute("DELETE FROM orders WHERE user_id = ? AND id = ?", [user_id, order_id])
+    # Adds a new item to the shop
+    #
+    def add_to_shop(title, price, amount)
+        db = connect_to_db("db/storprojekt.db")
+        db.execute("INSERT INTO items(title, price, amount) VALUES (?,?,?)", [title, price, amount])
+    end
+
+    # Updates an item in the shop
+    #
+    def update_shop(title, price, amount)
+        db = connect_to_db("db/storprojekt.db")
+        db.execute("UPDATE items SET price = ?, amount = ? WHERE title = ?", [price, amount, title])
+    end
+
+    # Deletes an item from the shop
+    #
+    def delete_from_shop(title)
+        db = connect_to_db("db/storprojekt.db")
+        db.execute("DELETE FROM items WHERE title = ?", title)
+    end
+
+    # Deletes an order
+    #
+    def deliver(user_id, order_id)
+        db = connect_to_db("db/storprojekt.db")
+        db.execute("DELETE FROM orders WHERE user_id = ? AND id = ?", [user_id, order_id])
+    end
 end
